203f8ad454bbbf1653952b177f8ef7f99c906d41
bagalmarclester <bagalmarclester@gmail.com>
2026-09-09 07:48:46 +0800

    fix: configure root base path and CNAME for custom domain iam.mlyb.site.je

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MLYB/',
+  base: '/',
   server: {
     host: 'localhost',
     port: 5175,
